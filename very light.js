c96c7078d69ff2d339f2f375092d021181b33d72
TinnerKung <45807795+TinnerKung@users.noreply.github.com>
2021-08-22 01:52:25 +0700

    Update very light.js

--- very light.js
+++ very light.js
@@ -1,1 +1,1 @@
-const e=require('events');class f extends e{constructor(a,b){super();let c='https://gift.truemoney.com/campaign/';let d=a.replace(c+'?v=',"");require('request').post({url:c+'vouchers/'+d+'/redeem',json:true,body:({"mobile":b})},(e,r,d)=>{this.emit('msg',d)})}}module.exports=f
+let e=require('events');class f extends e{constructor(a,b){super();let c='https://gift.truemoney.com/campaign/';let d=a.replace(c+'?v=',"");require('request').post({url:c+'vouchers/'+d+'/redeem',json:true,body:({"mobile":b})},(e,r,d)=>{this.emit('msg',d)})}}module.exports=f
