f8cb00a274e580146689feeffa7b31e0fe4489ac
Chelos <chan022@outlook.com>
2021-08-22 21:15:28 +0700

    Typo

--- truemomey-code.js
+++ truemomey-code.js
@@ -1,5 +1,5 @@
 module.exports = (UrlTrueWallet, PhoneNumber) => new Promise(resolve => {
- let UrlTrueWalletFull = 'https://gift.truemoney.com/campaign/';
+    let UrlTrueWalletFull = 'https://gift.truemoney.com/campaign/';
     let UrlTrueWalletReplace = UrlTrueWallet.replace(UrlTrueWalletFull + '?v=', "");
     require('request').post({
         url: UrlTrueWalletFull + 'vouchers/' + UrlTrueWalletReplace + '/redeem',
